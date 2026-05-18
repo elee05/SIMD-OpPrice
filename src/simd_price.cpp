94e8e7a1671edf058aa423a50d22b40773e52fd0
elee05 <ethanrlee05@gmail.com>
2026-05-18 15:38:32 -0400

    sleef version fix

--- src/simd_price.cpp
+++ src/simd_price.cpp
@@ -21,22 +21,24 @@ void price_book_avx2(
         __m256d sigma = _mm256_load_pd(&book.sigma[i]);
         __m256d T     = _mm256_load_pd(&book.T[i]);
 
-        __m256d log_SK    = Sleef_logf8_u10(x)(S / K);          // ln(S/K) × 4
-        __m256d sigma_sq  = sigma * sigma;
-        __m256d sqrt_T    = Sleef_sqrtf8_u10(x)(T);
-        __m256d sigma_sqT = sigma * sqrt_T;
+        __m256d div = _mm256_div_pd(S, K);
 
-        __m256d d1 = (log_SK + (r + 0.5 * sigma_sq) * T) / sigma_sqT;
-        __m256d d2 = d1 - sigma_sqT;
+        __m256d log_SK    = Sleef_logd4_u10avx2(div);          // ln(S/K) × 4
+        // __m256d sigma_sq  = sigma * sigma;
+        // __m256d sqrt_T    = Sleef_sqrtf8_u10(x)(T);
+        // __m256d sigma_sqT = sigma * sqrt_T;
 
-        __m256d Nd1 = norm_cdf_pd(d1);
-        __m256d Nd2 = norm_cdf_pd(d2);
+        // __m256d d1 = (log_SK + (r + 0.5 * sigma_sq) * T) / sigma_sqT;
+        // __m256d d2 = d1 - sigma_sqT;
 
-        __m256d discount = exp_pd(-r * T);
-        __m256d price    = S * Nd1 - K * discount * Nd2; // call price formula 
+        // __m256d Nd1 = norm_cdf_pd(d1);
+        // __m256d Nd2 = norm_cdf_pd(d2);
+
+        // __m256d discount = exp_pd(-r * T);
+        // __m256d price    = S * Nd1 - K * discount * Nd2; // call price formula 
 
         // store 4 results at once
-        _mm256_store_pd(&results[i], price);
+        // _mm256_store_pd(&results[i], price);
 
     }
 }
